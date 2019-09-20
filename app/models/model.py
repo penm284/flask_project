0147fa7041df0913942038f026a5f2365fec673c
penm284 <2020pnavarro@dwight.edu>
2019-09-20 08:05:57 -0400

    Second push

---
+++ app/models/model.py
@@ -0,0 +1,27 @@
+# ##function take a string, return the string but reversed
+# def flipit(string):
+#     newstring = ""
+#     for n in string:
+#         newstring = n + newstring
+#     return newstring
+#
+# print(flipit("hello"))
+#
+# ##function takes a string, returns all caps version
+# def shout(string):
+#     return string.upper()
+
+
+def gen (DOB):
+    message = ""
+    if DOB < 1945:
+        message = "You are part of the silent generation"
+    if DOB > 1977 and DOB < 1995:
+        message = "You are a millenial"
+    if DOB <=1976 and DOB>1965:
+        message = "You are part of gen X"
+    if DOB <=1964 and DOB>1946:
+        message = "You are a baby boomer"
+    return message
+
+print (gen(DOB))
--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,7 @@ from app.models import model, formopener
 def index():
     return render_template('index.html')
 
-@app.route('/secret')
-def secret():
-    return "you found the secret"
 
-
-@app.route('/comp')
-def comp():
-    return "comp sci"
 
 
 @app.route('/results', methods = ["post","get"])
@@ -22,6 +15,6 @@ def results():
     #process data from form
     userdata = dict(request.form)
     print(userdata)
-    nickname= userdata ['nickname']
-    output = model.flipit(nickname)
-    return output
+    generation= userdata ['generation']
+    output = model.gen(generation)
+    return "generation"
